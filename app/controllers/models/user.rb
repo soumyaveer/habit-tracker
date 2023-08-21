d7a0ffb63e35b46cd41966a9d18db2601eddf92a
Soumya Veer <sveer@easypost.com>
2023-08-21 16:55:53 -0700

    Add validations to model

--- app/controllers/models/user.rb
+++ app/controllers/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_many :user_habits, dependant: :destroy
   has_many :habits, through: :user_habits
+
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :name, :email, uniqueness: true
+  validates :password, presence: true
 end
