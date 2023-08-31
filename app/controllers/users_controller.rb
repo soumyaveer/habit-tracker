c78e39926c379573d6b29b4a7ae7e7a435655bbb
Soumya Veer <veer.soumya@gmail.com>
2023-08-31 16:23:24 -0700

    Complete User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
      users = User.all
      json users: users.as_json
    end
+
+   post "/api/users" do
+      user = User.new(
+        name: json_request_body[:name],
+        email: json_request_body[:email],
+        password: json_request_body[:password],
+      )
+
+      if user.save
+        json user.as_json
+      else
+        status(422)
+        user_json = user.as_json
+        user_json[:errors] = user.errors.full_messages
+
+        json user_json
+      end
+   end
 end
